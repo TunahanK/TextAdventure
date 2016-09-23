0c119239db8b25ddc95869fae95d1462b90e10d7
TunahanK <t.kasimay15@gmail.com>
2016-09-23 09:00:35 +0100

    ugzf

--- src/Model/Weapon3.java
+++ src/Model/Weapon3.java
@@ -4,7 +4,7 @@ public class Weapon3 extends Weapon {
 
     public Weapon3(){
         this.name = "Der heilige Gurkenschäler";
-        this.level = 5;
+        this.level = 4;
         this.attack = 50;
     }
 }
